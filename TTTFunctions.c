9ca00fc6f94054171cd58ae8a4c8c506e2fab06c
SuperKuo <jerrykuo820@gmail.com>
2018-12-23 01:02:05 -0800

    Complete Version

--- TTTFunctions.c
+++ TTTFunctions.c
@@ -10,15 +10,16 @@ void game_manager(void) {
         PVP_game();
     } else if (!strcmp(game_mode, "PVE")) {
         PVE_game();
-    } else if (!strcmp(game_mode, "EXIT")) {
-        return;
+    } else if (!strcmp(game_mode, "EXIT") || !strcmp(game_mode, "ERROR")) {
+        //Quits in case of error instead of behaving weirdly
+        exit(0);
     }
     if (restart()) {
         game_manager();
     }
 }
 
-int AI_manager(char board[3][3], int depth) {
+int AI_manager(char board[3][3]) {
     FILE *evaluation_out;
     char imaginary_board[3][3] = {
             {'1', '2', '3'},
@@ -35,7 +36,7 @@ int AI_manager(char board[3][3], int depth) {
         printf("Cannot Open File\n");
         exit(EXIT_FAILURE);
     }
-    fprintf(evaluation_out, "# 1 2 3 4 5 6 7 8 9\n\n"); //Prints header for easier clarification and sorting.
+    fprintf(evaluation_out, "# 0 1 2 3 4 5 6 7 8 9\n\n"); //Prints header for easier clarification and sorting.
 
 
     see_the_future(imaginary_board, evaluation_out, 3);
@@ -51,7 +52,7 @@ void see_the_future(char imaginary_board[3][3], FILE *evaluation_out, int depth)
     char temp;
 
     if (round_depth == 1) {
-        fprintf(evaluation_out, "+ ");
+        fprintf(evaluation_out, "+ 0 ");
         board_analysis(imaginary_board, evaluation_out, 1);
         fprintf(evaluation_out, "\n\n");
     }
@@ -66,9 +67,9 @@ void see_the_future(char imaginary_board[3][3], FILE *evaluation_out, int depth)
                     round_depth++;
                     imaginary_board[row][column] = ((round_depth + 1) % 2 + 1 == 1) ? 'X' : 'O';
                     if (round_depth == 2) {
-                        fprintf(evaluation_out, "- ");
+                        fprintf(evaluation_out, "- %d ", row * 3 + column + 1);
                     } else
-                        fprintf(evaluation_out, "~ ");
+                        fprintf(evaluation_out, "~ %d ", row * 3 + column + 1);
 
                     board_analysis(imaginary_board, evaluation_out, (round_depth + 1) % 2 + 1);
                     fprintf(evaluation_out, "\n");
@@ -84,30 +85,33 @@ void see_the_future(char imaginary_board[3][3], FILE *evaluation_out, int depth)
 }
 
 void sort_AI_results(FILE *evaluation_out, int move[9]) {
-    int minimax[9];
+    int minimax[9], line, temp = 0, skip = 0;
     char header;
 
     if (fseek(evaluation_out, 0L, SEEK_SET) != 0) {
         printf("Failure to rewind files\n");
     }
 
-    while (fscanf(evaluation_out, " %c %d %d %d %d %d %d %d %d %d", &header, &minimax[0], &minimax[1], &minimax[2],
-                  &minimax[3], &minimax[4], &minimax[5], &minimax[6], &minimax[7], &minimax[8]) != EOF) {
+    while (fscanf(evaluation_out, " %c %d %d %d %d %d %d %d %d %d %d", &header, &line, &minimax[0], &minimax[1],
+                  &minimax[2], &minimax[3], &minimax[4], &minimax[5], &minimax[6], &minimax[7], &minimax[8]) != EOF) {
         if (header == '+') {
             for (int i = 0; i <= 8; i++) {
                 move[i] = minimax[i];
             }
         } else if (header == '-') {
             for (int i = 0; i <= 8; i++) {
-                if (minimax[i] == 1) {
-                    move[i] = 1;
+                if (minimax[i] == 1 || minimax[i] == 2) {
+                    move[line - 1] = 1;
+                    skip = FALSE;
                     break;
                 }
+                skip = TRUE;
             }
-        } else if (header == '~') {
+            temp = line;
+        } else if (header == '~' && skip) {
             for (int i = 0; i <= 8; i++) {
                 if (minimax[i] == 2) {
-                    move[i] = 2;
+                    move[temp - 1] = 2;
                     break;
                 }
             }
@@ -129,12 +133,10 @@ void board_analysis(char imaginary_board[3][3], FILE *evaluation_out, int player
             if (imaginary_board[row][column] == 'O' || imaginary_board[row][column] == 'X' ||
                 check_win(player, imaginary_board)) {
                 fprintf(evaluation_out, "5 ");
-                //print_board(imaginary_board, evaluation_out);
             } else {
                 temp = imaginary_board[row][column];
                 imaginary_board[row][column] = (player == 1) ? 'X' : 'O';
                 fprintf(evaluation_out, "%d ", check_win(player, imaginary_board));
-                //print_board(imaginary_board, evaluation_out);
                 imaginary_board[row][column] = temp;
             }
         }
@@ -162,7 +164,7 @@ int return_move(int move[9]) {
 }
 
 void PVE_game(void) {
-    int AI_input = 0, player = 0, depth = 2;
+    int AI_input = 0, player = 0;
     int winner = FALSE;
     char board[3][3] =
             {
@@ -171,23 +173,22 @@ void PVE_game(void) {
                     {'7', '8', '9'}
             };
 
-    printf("Please enter the difficulty you want (the depth of AI):\n");
-    //scanf("%d", &depth);
-
     for (int i = 0; i < 9 && winner == FALSE; i++) {
         print_board(board, stdout);
         player = i % 2 + 1;
         if (player == 1) {
             while (coordinates_validation(player, board) == -1);
         } else if (player == 2) {
-            AI_input = AI_manager(board, depth);
-            board[--AI_input / 3][AI_input % 3] = 'O';
+            AI_input = AI_manager(board);
+            board[(AI_input - 1) / 3][(AI_input - 1) % 3] = 'O';
+            printf("\nThe AI has made a move");
         }
         winner = check_win(player, board);
     }
-    print_board(board, stdout);
     if (winner == FALSE)
         printf("\nDraw\n");
+    else if(winner == 2)
+        printf("\n The AI has beaten you");
     else
         printf("\nPlayer %d, YOU WON!\n", winner);
 }
@@ -239,8 +240,7 @@ char *main_menu(void) {
     printf("q  , Quit       :  Quit the game\n");
     printf("\n......................................................\n");
     printf("%s", input_prompt);
-    //fgets(user_input, INPUT_STRING, stdin); //gets user input
-    strcpy(user_input, "pve\n");
+    fgets(user_input, INPUT_STRING, stdin); //gets user input
     input_validation(user_input);
 
     if (!strcmp(user_input, "PvP") || !strcmp(user_input, "pvp") || !strcmp(user_input, "Game Mode 1")) {
--- TTTFunctions.h
+++ TTTFunctions.h
@@ -2,38 +2,42 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
-#define INPUT_STRING 32
-#define SHORT_STRING 16
-#define TRUE 1
-#define ERROR_VALUE -99
-#define FALSE 0
+
 #pragma importf "TTTFunction.c"
 
-void game_manager(void);
+#define INPUT_STRING 32
+#define SHORT_STRING 16
+#define ERROR_VALUE -99
+#define TRUE 1
+#define FALSE 0
 
-char *main_menu(void);
+
+void PVE_game(void);
 
 void PVP_game(void);
 
 void print_board(char board[3][3], FILE *stream);
 
-int restart(void);
-
-void input_validation(char *user_input);
-
-int check_win(int player, char board[3][3]);
-
-void PVE_game(void);
-
-int coordinates_validation(int player, char board[3][3]);
-
-int AI_manager(char board[3][3], int depth);
-
-void board_analysis(char imaginary_board[3][3], FILE *evaluation_out, int player);
-
-int return_move(int move[9]);
+void game_manager(void);
 
 void see_the_future(char imaginary_board[3][3], FILE *evaluation_out, int depth);
 
+void board_analysis(char imaginary_board[3][3], FILE *evaluation_out, int player);
+
 void sort_AI_results(FILE *evaluation_out, int move[9]);
+
+void input_validation(char *user_input);
+
+int restart(void);
+
+int check_win(int player, char board[3][3]);
+
+int AI_manager(char board[3][3]);
+
+int return_move(int move[9]);
+
+int coordinates_validation(int player, char board[3][3]);
+
+char *main_menu(void);
+
 
